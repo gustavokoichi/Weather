6010d55108048551278893f2e561a7ce9d252ef2
Gustavo Koichi <guga_koichi@hotmail.com>
2023-02-12 16:33:45 -0300

    Returning json info to my terminal

--- weather.py
+++ weather.py
@@ -1,6 +1,7 @@
 import argparse
+import json
 from configparser import ConfigParser
-from urllib import parse
+from urllib import parse, request
 
 BASE_WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather"
 
@@ -52,6 +53,15 @@ def read_user_cli_args():
 
 
 def build_weather_query(city_input, imperial=False):
+    """Builds the URL for an API request to OpenWeather's API.
+
+    Args:
+        city_input (List[str]): Name of a city collected by argparse
+        imperial (bool): Whether or not to use imperial units for temperature
+
+    Returns:
+        str: URL formatted for a call to OpenWeather's city name
+    """
     api_key = _get_api_key()
     city_name = " ".join(city_input)
     url_encoded_city_name = parse.quote_plus(city_name)
@@ -62,8 +72,27 @@ def build_weather_query(city_input, imperial=False):
     )
     return url
 
+
+def get_weather_data(query_url):
+    """Makes an API request to a URL and returns the data as a Python object.
+
+    Args:
+        query_url (str): URL formatted for OpenWeather's city name endpoint
+    
+    response: make an HTTP GET request to the query_url parameter and saves the result as response
+    data: extracts the data from response
+    
+    Returns:
+        dict: Weather information for a specific city
+    """
+    response = request.urlopen(query_url)
+    data = response.read()
+    return json.loads(data)
+
+
 if __name__ == "__main__":
     user_args = read_user_cli_args()
     query_url = build_weather_query(user_args.city, user_args.imperial)
-    print(query_url)
+    weather_data = get_weather_data(query_url)
+    print(weather_data)
 
