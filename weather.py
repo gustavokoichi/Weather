595ba1fa989f28b401ca4e4f101017e3460edf8e
Gustavo Koichi <guga_koichi@hotmail.com>
2023-02-12 16:21:45 -0300

    Build a url given city and unit

--- weather.py
+++ weather.py
@@ -1,5 +1,8 @@
 import argparse
 from configparser import ConfigParser
+from urllib import parse
+
+BASE_WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 # Starting a function with _ indicates that it should be non-public
 
@@ -29,14 +32,14 @@ def read_user_cli_args():
     """
 
     parser = argparse.ArgumentParser(
-        description="Gets weather and temperature from a city"
+        description="Gets weather and temperature from a city."
     )
 
     parser.add_argument(
         "city",
         nargs="+",
         type=str,
-        help="Enter city name: ",
+        help="Enter city name",
     )
 
     parser.add_argument(
@@ -48,6 +51,19 @@ def read_user_cli_args():
     return parser.parse_args()
 
 
+def build_weather_query(city_input, imperial=False):
+    api_key = _get_api_key()
+    city_name = " ".join(city_input)
+    url_encoded_city_name = parse.quote_plus(city_name)
+    units = "imperial" if imperial else "metric"
+    url = (
+        f"{BASE_WEATHER_API_URL}?q={url_encoded_city_name}"
+        f"&units={units}&APPID={api_key}"
+    )
+    return url
+
 if __name__ == "__main__":
     user_args = read_user_cli_args()
-    print(user_args.city, user_args.imperial)
+    query_url = build_weather_query(user_args.city, user_args.imperial)
+    print(query_url)
+
